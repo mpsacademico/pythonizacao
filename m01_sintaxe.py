e19a1082e4f5b4bd4a116f7422c4d8c012246727
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-06-06 19:34:13 -0300

    Inclusão de apontamentos sobre a sintaxe da linguagem

---
+++ m01_sintaxe.py
@@ -0,0 +1,13 @@
+# codig=UTF-8
+
+print("Comentários de Linha Única:") # O resto de uma linha após # é um comentário
+# esta linha é um comentário de uma linha e será ignorado pelo interpretador
+
+print("Comentários Funcionais:")
+print("#!/usr/bin/env python") # Definição do interpretador do programa em sistemas Linux
+print("# -*- coding: latin1 -*-") # UTF-8 ou latin1: acentuação da Língua Portuguesa (padrão ASCII)
+print("# codig = UTF-8 ") # Realiza a mesma função que o comentário funcional anterior
+
+soma = 2 + 2 \
++ 6 # A contra-barra ou a vírgula permitem a linha do código-fonte
+print("A soma resulta em", soma) # a vírgula insere espaços entre os parâmetros informados
