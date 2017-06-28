5fae5a28d2fac6fb16b1c008cf14807e40647d2c
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-06-27 23:59:17 -0300

    Uso de módulo preliminar do módulo tabuada

--- parte2/m008_modulos.py
+++ parte2/m008_modulos.py
@@ -1,3 +1,21 @@
 # codig=UTF-8
 
 import tabuada
+import sys # This module provides access to some variables used or maintained by the interpreter and to functions that interact strongly with the interpreter. It is always available.
+
+from pprint import pprint
+
+'''
+print(tabuada.resultado)
+
+tabuada.calcular(5)
+print(tabuada.resultado)
+
+tabuada.calcular(6)
+print(tabuada.resultado)
+'''
+
+print(sys.path)
+pprint(sys.path)
+
+
