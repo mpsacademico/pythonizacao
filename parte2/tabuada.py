77497b44b2f4ce7a84396f085b15b53741688c80
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-06-27 23:58:48 -0300

    Adição de comentários explicativos sobre o módulo tabuada

--- parte2/tabuada.py
+++ parte2/tabuada.py
@@ -1,10 +1,23 @@
 # codig=UTF-8
 
+'''
+Olá, eu sou um módulo. Meu nome é tabuada (pode deixar o .py para lá).
+
+Eu sei que estão precisando de mim quando me chamam. Alguém usa a instrução import seguida pelo meu nome.
+É assim que é feito para chamar qualquer módulo por essas bandas.
+
+Quando eu sou importado pela primeira vez algo incrível acontece.
+Eu mudo de sobrenome, deixo de ser .py e viro .pyc ou .pyo. Geralmente eu vou morar dentro de um diretório chamado __pycache__.
+Dizem que esse processo chama-se compilação. Parece que a compilação faz com que eu chegue mais rápido das próximas vezes que for chamado.
+
+Em seguida, eu sou executado.
+'''
+
 resultado = []
 
 def calcular(numero):
-	resultado = []	
-	for x in range(0,11):
+	del resultado[:]
+	for x in range(0, 11):		
 		resultado.append(numero*x)
 		
-print(resultado)
+print("tabuada.py diz: - Eu sou um módulo e fui carregado e executado!")
