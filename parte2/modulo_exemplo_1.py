c0a660c2ed6935253cfe9d5b3064b5951b75a8cb
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-06-29 19:44:18 -0300

    Criação de módulos de exemplo

---
+++ parte2/modulo_exemplo_1.py
@@ -0,0 +1,13 @@
+# codig=UTF-8
+
+print(__name__)
+
+variavel = 1
+
+def funcao():
+	return 2
+
+if __name__ == "__main__":
+	print("Esse módulo é o principal")	
+else:
+	print(__name__+" não é um módulo principal")
---
+++ parte2/modulo_exemplo_2.py
@@ -0,0 +1,13 @@
+# codig=UTF-8
+
+print(__name__)
+
+variavel = 3
+
+def funcao():
+	return 4
+
+if __name__ == "__main__":
+	print("Esse módulo é o principal")
+else:
+	print(__name__+" não é um módulo principal")
