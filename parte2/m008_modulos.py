b56c612c659bca5f03790f567d331311f62daab7
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-06-29 19:55:36 -0300

    Adição de apontamentos sobre módulos

--- parte2/m008_modulos.py
+++ parte2/m008_modulos.py
@@ -1,21 +1,49 @@
 # codig=UTF-8
 
+print("MÓDULOS")
+'''
+- arquivos fonte que podem ser importados
+- reaproveitamento e facilidade de manutenção
+
+Primeira importação de um módulo:
+- é localizado pelo interpretador usando o PYTHONPATH (sys.path) - lista de diretórios começando pelo do script principal
+- compilado e armazenado em arquivo .pyc ou .pyo no diretório __pycache__
+- executado (próximas vezes necessita de reload())
+
+São objetos Singleton - uma instância na memória disponível globalmente
+
+if __name__ == "__main__": - VERDADEIRO no módulo principal
+
+'''
+# CARREGAMENTO DE MÓDULOS 
+
+# importa absoluta - import MÓDULO (módulo - nome do arquivo Python sem extensão)
+import modulo_exemplo_1
+print(modulo_exemplo_1.variavel) # a identificação do módulo evita ofuscamentos de variáveis e funções
+print(modulo_exemplo_1.funcao())
+
+# importação relativa - from MÓDULO import ESTRUTURA (estrutura = variável, função etc.)
+from modulo_exemplo_2 import * # asterisco importa tudo que está definido no módulo
+'''
+from modulo_exemplo_2 import variavel
+from modulo_exemplo_2 import funcao
+'''
+print(variavel)
+print(funcao())
+
+# namespace e DocStrings próprios
 import tabuada
-import sys # This module provides access to some variables used or maintained by the interpreter and to functions that interact strongly with the interpreter. It is always available.
+print(tabuada.resultado)
+tabuada.calcular(5)
+print(tabuada.resultado)
+tabuada.calcular(6)
+print(tabuada.resultado)
+
+# exemplos de módulos nativos do Python
+import sys # este módulo provê acesso a algumas variáveis usadas ou mantidas pelo interpretador e funções que interagem fortemente com o interpretador
 
 from pprint import pprint
 
-'''
-print(tabuada.resultado)
-
-tabuada.calcular(5)
-print(tabuada.resultado)
-
-tabuada.calcular(6)
-print(tabuada.resultado)
-'''
-
-print(sys.path)
-pprint(sys.path)
-
-
+sys.exit() # para a execução do programa Python
+print(sys.path) # uma lista de strings que especificam os caminhos para os módulos
+#pprint(sys.path)
